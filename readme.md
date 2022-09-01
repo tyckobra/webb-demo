c13193d7d5834fc4304f165a4e202d6d27120c7a
Jens Andreasson <jens.andreasson@ntig.se>
2022-09-01 13:54:05 +0200

    🎉 all the stuff

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Detta är ett exempel
+
+Det här är ett exempel på alla delarna vi kommer att jobba med och få med under året som kommer.
+Ett smakprov!
+
+## Stack
+
+[Node](https://nodejs.org/en/)
+[Express](https://expressjs.com/)
+[MySQL](https://www.mysql.com/)
+
+## Frontend
+
+[Nunjucks](https://mozilla.github.io/nunjucks/)
+[Sass](https://sass-lang.com/)
+
+# Kommandon
+
+```bash
+mkdir webb-demo
+cd webb-demo
+npm install
+cp .env-example .env
+npm run dev
+```
+
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const pool = require('../utils/database');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+
+    try {
+        const [rows] = await pool
+        .promise()
+        .query(`SELECT * from demo`);
+    
+        console.log(rows);
+
+        res.render('index.njk', {
+            title: 'Kursdemo',
+            intro: 'En demosida för att ge ett litet smakprov på tekniken och vad den kan göra.',
+            rows: rows
+        });
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+});
+
+module.exports = router;
